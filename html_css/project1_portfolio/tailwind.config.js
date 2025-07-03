6f6cf4d61bdb3e739a747ac909570e74d8321183
Debasish <debasishdas2439@gmail.com>
2025-07-03 09:52:05 +0530

    changes

--- html_css/project1_portfolio/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./dist/**/*.html",    // <-- include all HTML in dist
-    "./src/**/*.{js,css}", // if you ever import Tailwind classes in JS/CSS
-  ],
-  theme: { extend: {} },
-  plugins: [],
-};
