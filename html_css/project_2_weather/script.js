b06ad8bfa618db60b964f0c08ea1a73e7e25fae4
Debasish <debasishdas2439@gmail.com>
2025-07-07 13:19:41 +0530

    weather app done

--- html_css/project_2_weather/script.js
+++ html_css/project_2_weather/script.js
@@ -1,21 +1,28 @@
 const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=`
 const apiKey = `&appid=cab045dab0d75a23796e0a7732e5483e&units=metric`
+
+const weatherIcon = document.getElementById('wi')
+const weatherP = document.getElementById('weather-p')
+const popUp = document.querySelector('.toast')
 //getting info from the input
 
 const searchBar = document.getElementById('search-bar')
- searchBar.addEventListener('keydown', event => {
-    if (event.key == 'Enter') {
-        const city = event.target.value;  
-        checkWeather(city)    
+searchBar.addEventListener('keydown', event => {
+  if (event.key == 'Enter') {
+    const city = event.target.value
+    if (city === undefined || city === null || city == '') {
+      popUp.style.display = 'block'
+      setTimeout(() => {
+        popUp.style.display = 'none'
+      }, 3500)
     }
+    checkWeather(city)
+  }
 })
-
-
-
 
 async function checkWeather (city) {
   try {
-    const rseponse = await fetch(apiURL+city+apiKey)
+    const rseponse = await fetch(apiURL + city + apiKey)
     const data = await rseponse.json()
     document.getElementById('city').innerText = data.name
     document.getElementById('temp').innerHTML = `${Math.round(
@@ -23,9 +30,31 @@ async function checkWeather (city) {
     )}°C`
     document.getElementById('humidity-id').innerHTML = data.main.humidity + '%'
     document.getElementById('wind-val').innerHTML = `${data.wind.speed} km/h`
+
+    if (data.weather[0].main == 'Clouds') {
+      weatherIcon.src = './assets/cloud-50.png'
+      weatherP.innerHTML = 'Clouds'
+    } else if (data.weather[0].main == 'Clear') {
+      weatherIcon.src = './assets/sun.png'
+      weatherP.innerHTML = 'Clear'
+    } else if (data.weather[0].main == 'Rain') {
+      weatherIcon.src = './assets/rain.png'
+      weatherP.innerHTML = 'Rain'
+    } else if (data.weather[0].main == 'Drizzle') {
+      weatherIcon.src = './assets/heavy-rain-50.png'
+      weatherP.innerHTML = 'Drizzle'
+    } else if (data.weather[0].main == 'Mist') {
+      weatherIcon.src = './assets/mist.png'
+      weatherP.innerHTML = 'Mist'
+    }
+
+    document.querySelector('.weather').style.display = 'block'
   } catch (error) {
-    console.log('error--> ', error)
+    // console.log('error--> ', error)
+    popUp.style.display = 'block'
+      setTimeout(() => {
+        popUp.style.display = 'none'
+      }, 3500)
+    
   }
 }
-
-
