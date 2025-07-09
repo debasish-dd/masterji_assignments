079913bc8c4c94aaf038478344c8f87c25c4d36a
Debasish <debasishdas2439@gmail.com>
2025-07-09 22:11:37 +0530

    made the website looks good, js needs to be done

--- javascript/todo-app/script.js
+++ javascript/todo-app/script.js
@@ -1,1 +1,30 @@
+const todoLists = document.getElementById("todo-lists"); 
+const input = document.querySelector(".input");
+const addBTN = document.querySelector(".add-btn");
+
+addBTN.addEventListener("click" , (e)=>{
+    
+    e.preventDefault();
+    addTODO();
+})
+
+function addTODO(){
+    const todoText = input.value.trim()
+    if(todoText.length>0){
+        createTODO(todoText);
+        input.value = ''
+    }
+    
+    
+}
+
+function createTODO(todo){
+    const todoLi = document.createElement("li");
+    todoLi.innerText = todo;
+    todoLists.append(todoLi)
+
+}
+
+
+
 
