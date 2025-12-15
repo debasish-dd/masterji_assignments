0782759fd7fcfb864b3517c5596408b91edd9327
Debasish <debasishdas2439@gmail.com>
2025-12-15 11:58:43 +0530

    added file upload system

---
+++ rag-website/app/components/file-upload.js
@@ -0,0 +1,102 @@
+"use client";
+import React, { useState, useCallback } from "react";
+import { useDropzone } from 'react-dropzone'
+import { ToastContainer, toast , Bounce } from 'react-toastify';
+
+
+export default function FileUpload() {
+
+    const onDrop = useCallback(acceptedFiles => {
+        // Do something with the files
+        if (acceptedFiles.length > 0) {
+            toast.success('The file has uploaded succesfully!', {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: false,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "dark",
+                transition: Bounce,
+            });
+            console.log('files-->', acceptedFiles);
+        }
+
+    }, [])
+    const onDropRejected = useCallback(rejectedFiles => {
+        // Do something with the files
+        if (rejectedFiles.length > 0) {
+            const tooManyFiles = rejectedFiles.some(file => file.errors.some(e => e.code === "too-many-files"));
+            const fileTooLarge = rejectedFiles.some(file => file.errors.some(e => e.code === "file-too-large"));
+
+            if (tooManyFiles) {
+
+                toast.error('Too Many Files!!', {
+                    position: "top-right",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: false,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "dark",
+                    transition: Bounce,
+                });
+            }
+            if (fileTooLarge) {
+
+                toast.error('File size should be less than 2.5MB!!', {
+                    position: "top-right",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: false,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "dark",
+                    transition: Bounce,
+                });
+            }
+
+        }
+
+    }, [])
+    const { getRootProps, getInputProps, isDragActive } = useDropzone({
+        onDrop,
+        onDropRejected,
+        maxFiles: 1,
+        maxSize: 1024 * 1024 * 2.5, // 2.5 MB 
+        accept: {
+            'application/pdf': [],
+        }
+
+    })
+    return (
+        <div className={`rounded-lg border-dashed border-2 h-45 m-5 flex justify-center items-center ${isDragActive ? "border-solid border-orange-500 bg-orange-300/30" : "hover:border-solid hover:border-orange-500"} `} {...getRootProps()}>
+            <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick={false}
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="dark"
+                transition={Bounce}
+            />
+            <input {...getInputProps()} />
+            {
+                isDragActive ?
+                    <p>Drop the files here ...</p> :
+                    <div className="felx flex-col items-center justify-center text-center">
+                        <p>Drag and drop files here</p>
+                        <button className="mt-2 p-2 bg-orange-500 text-white rounded">Browse Files</button>
+                    </div>
+            }
+        </div>
+    )
+
+}
---
+++ rag-website/app/page.js
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import FileUpload from "./components/file-upload";
+
+export default function Home() {
+  return (
+   <div className="flex gap-3 w-screen min-h-screen">
+     <div className=" lg:w-[35vw] w-[45vw] border-r-2">
+      <FileUpload/>
+     </div>
+     <div className="lg:w-[65vw] w-[55vw]">
+    2
+     </div>
+   </div>
+  );
+}
